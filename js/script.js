2cfcde9653426019e655ddad5141ee743cf3513d
Simone <info@simoneicardi.com>
2024-12-16 11:51:03 +0100

    esempi uso variabili con numeri e stringhe

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// dichiarazione variabile 
+
+// dichiarazione nome utente
+let firstName;
+
+// dichiarazione cognome utente
+let lastName;
+
+
+
+// assegnare nome e cognome utente
+firstName = "Paola";
+lastName = "Verdi";
+
+// variabile di nome completo
+// let nomeCompleto = firstName + " " + lastName;
+
+// console.log(nomeCompleto);
+
+// saluto per l'utente
+let salutoUser = "Ciao il mio nome è " + firstName + " " + lastName + ", e vi saluto tutti";
+
+let salutoUser2 = `Ciao il mio nome è ${firstName} ${lastName}, e vi saluto tutti `;
+
+console.log(salutoUser2);
+
+// prove con i numeri
+let somma = 2 + 5;
+
+console.log("il valore di somma è: ", somma);
+
+let sottrazione = somma - 3;
+
+console.log("il valore di sottrazione è: ", sottrazione);
+
+let moltiplicazione = somma * sottrazione;
+
+console.log("il valore di moltiplicazione è: ", moltiplicazione);
+
+
+
+
+
+
