cb3d9b480a7c0da9e30aebd0f4dbc980f8bfc87c
Simone <info@simoneicardi.com>
2024-12-18 12:14:17 +0100

    ouput possibile fizz buzz

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@
 for (let i = 200; i >= 100; i--) {
     console.log("il valore di i è: ", i);
 }
+
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+
