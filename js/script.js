591d3113ffbf9c6860850ba5bcf151a2411c5557
Simone <info@simoneicardi.com>
2024-12-19 10:47:14 +0100

    es. traversing array di stringhe

--- js/script.js
+++ js/script.js
@@ -1,30 +1,13 @@
-// stampiamo i valori di i da 100 a 200
-// for (let i = 100; i <= 200; i++) {
-//     console.log("il valore di i è: ", i);
-// }
+const iscritti = ["Luca", "Marco", "Paolo"];
 
+//  estriamo facendo un log di ogni singolo elemento dell'array iscritti
+for (let i = 0; i < iscritti.length; i++) {
 
-// stampiamo i valori di i da 200 a 100
-for (let i = 200; i >= 100; i--) {
-    console.log("il valore di i è: ", i);
+    let iscrittoIesimo = iscritti[i];
+
+    console.log(`${iscrittoIesimo} è nella posizione ${i}`);
+
 }
 
-// Scrivi un programma che stampi i numeri da 1 a 100,
-// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-// 1
-// 2
-// Fizz
-// 4
-// Buzz
-// Fizz
-// 7
-// 8
-// Fizz
-// Buzz
-// 11
-// Fizz
-// 13
-// 14
-// FizzBuzz
+
 
