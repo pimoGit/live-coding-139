a2a8473f41bb750b1f4060fb9e043d2e56ad054c
Simone <info@simoneicardi.com>
2024-12-20 12:52:24 +0100

    jsnack di conteggio

--- js/script.js
+++ js/script.js
@@ -1,42 +1,25 @@
-const cities = ['London', 'Shanghai', 'New York', 'Delhi', 'Kuala Lumpur'];
-const names = [];
-const numbers = [1, 2, 3];
-const colours = ['Red', 'Blue', 'Yellow'];
-const keys = ['q', 'w', 'e', 'r', 't', 'y'];
-const countries = ['Bolivia', 'Jordan', 'Greenland'];
-const fruits = ['Apple', 'Orange', 'Pear'];
+/*
+ JSnack 3
+ Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+ Il programma stampa la somma di tutti i numeri inseriti. 
+ 
+*/
 
-// 1. Aggiungi Fred all'array names
-names.push("Fred");
-console.log(names);
+// setto la variabile che conterrà la somma
+let sommaFor = 0;
 
-// 2. Aggiungi 4 alla fine dell'array numbers
-numbers.push(4);
-console.log(numbers);
+// Chiedo per 5 volte unnumero all'utente
+for (let i = 0; i < 5; i++) {
+    const nuovoNumero = parseInt(prompt('Inserisci un numero'));
 
-// 3. Aggiungi Rio all'inizio dell'array cities
-cities.unshift("Rio");
-console.log(cities);
-
-// 4. Rimuovi il primo colore dall'array colours
-colours.shift();
-console.log(colours);
-
-// 5. Rimuovi l'ultimo elemento dall'array keys
-keys.pop();
-console.log(keys);
-
-// 6. Rimuovi Jordan dall'array countries
-// countries.splice(1, 1);
-// ricavo l'indice in base al contenuto
-let indiceGiordania = countries.indexOf('Jordan');
-// cancelliamo l'elemento sull'indice ricavato
-countries.splice(indiceGiordania, 1);
-console.log(countries);
+    // output numeri inseriti con relativo indice
+    console.log(nuovoNumero, i);
 
 
+    // aggiungo al valore di somma il nuovo numero ad ogni giro del for (iterazione)
+    sommaFor += nuovoNumero;
+    // sommaFor = sommaFor + nuovoNumero;
+}
 
-// 7. Rimuovi l'ultimo frutto dall'array fruits e memorizzalo in pear qui sotto
-let pear = fruits.pop();
-console.log(fruits, pear);
-
+// output
+console.log('La somma dei numeri inseriti è ' + sommaFor);
