44994cd483916e846c8da3b5010712e37bec3c1e
Simone <info@simoneicardi.com>
2025-01-08 10:42:19 +0100

    primo es. uso funzioni

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-/*
- JSnack 3
- Il software deve chiedere per 5 volte all’utente di inserire un numero. 
- Il programma stampa la somma di tutti i numeri inseriti. 
- 
-*/
-
-// setto la variabile che conterrà la somma
-let sommaFor = 0;
-
-// Chiedo per 5 volte unnumero all'utente
-for (let i = 0; i < 5; i++) {
-    const nuovoNumero = parseInt(prompt('Inserisci un numero'));
-
-    // output numeri inseriti con relativo indice
-    console.log(nuovoNumero, i);
 
 
-    // aggiungo al valore di somma il nuovo numero ad ogni giro del for (iterazione)
-    sommaFor += nuovoNumero;
-    // sommaFor = sommaFor + nuovoNumero;
+
+const somma = sommaNumeri(49, 2);
+
+const somma2 = sommaNumeri(12, 22);
+
+
+const sommaSomme = sommaNumeri(somma, somma2)
+
+console.log(somma, somma2, sommaSomme); // 51
+
+console.log(risultato); // "ReferenceError: risultato is not defined
+
+
+
+
+
+
+// FUNCTIONS
+function sommaNumeri(num1, num2) {
+
+    const risultato = num1 + num2;
+
+    return risultato;
+
 }
-
-// output
-console.log('La somma dei numeri inseriti è ' + sommaFor);
