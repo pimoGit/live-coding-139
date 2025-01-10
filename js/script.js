d5a0acd6a36280e4894e5ff59172a46d0b763b4e
Simone <info@simoneicardi.com>
2025-01-10 11:42:06 +0100

    es. gestione condizionale elementi nel DOM

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
+// Il software deve chiedere all’utente il suo nome e il sesso con due prompt diversi.
+// Sulla pagina html deve apparire “Ciao <nome>”, 
+// il colore del nome deve essere azzurro o rosa a seconda del sesso inserito */
+
+
+let sexClass;
+
+// selezioniamo l'elemnto di output
 const element = document.getElementById("saluto");
 
-element.className += " titolo";
-
-// ma se non vogliamo perdere le altre classi...
-// element.className = element.classList + " titolo";
-
-// cambio colore senza classe
-// element.style.color = "red";
+console.log(element);
 
 
-// aggiungere del contenuto HTML
-element.innerHTML += '<p class="classetesto">Mio testo</p>';
 
+// chiediamo i dati allo User
+const nomeUser = prompt("inserisci il tuo nome");
+const sesso = prompt("inseisci il tuo sesso, M o F");
 
+console.log(nomeUser, sesso);
+
+// aggiungiamo il nomecome output
+element.innerHTML = `Ciao ${nomeUser}`;
+
+// gestiamo il nome della classe da associare in base al sesso
+if (sesso === "M") {
+    sexClass = "male";
+} else {
+    sexClass = "female"
+}
+
+console.log(sexClass);
+
+// associamo la classe giusta
+element.classList.add(sexClass);
