c99dea71ab064f003900e946294b0387bb446962
Simone <info@simoneicardi.com>
2025-01-13 10:48:33 +0100

    creazione lista con createElement

--- js/script.js
+++ js/script.js
@@ -8,18 +8,32 @@ const names = ['Qui', 'Quo', 'Qua', "paperino", "pluto"];
 // OUTPUT
 
 // inizializzazione della variabile di accumulo
-let items = '';
+// let items = '';
 
+// for (let i = 0; i < names.length; i++) {
+
+//     let nomeIesimo = names[i];
+
+//     items += `   <div class="classeNome">
+//                 <a href="#">
+//                     ${nomeIesimo}
+//                 </a>
+//             </div>`;
+
+//     console.log("all'iterazione numero: ", i, "items corrisposnde a: ", items);
+
+// }
+
+// // aggiunta reale alla pagina degli elementi
+// list.innerHTML = items;
+
+
+
+// Versione con createElement
 for (let i = 0; i < names.length; i++) {
-
-    let nomeIesimo = names[i];
-
-    items += `<li>${nomeIesimo}</li>`;
-
-    console.log("all'iterazione numero: ", i, "items corrisposnde a: ", items);
-
+    const li = document.createElement('li');
+    const collegamento = document.createElement('a');
+    li.appendChild(collegamento);
+    collegamento.append(names[i]);
+    list.appendChild(li);
 }
-
-
-
-list.innerHTML = items;
