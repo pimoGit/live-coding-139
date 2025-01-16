edada2a8c401045ca1960815929d96bf9f4a0b4c
Simone <info@simoneicardi.com>
2025-01-16 11:43:10 +0100

    es. base su oggetti letterali

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
-const inputUser = document.querySelectorAll('.inputUser');
+// creiamo il nostro oggetto
+const palla = {
+    'colore': ['red', 'blue'],
+    'tipo': 'pallina da golf',
+    'diametro': 3
+};
 
-console.log(inputUser);
+// const nomePorp = prompt("inserisci il nome di una prop della palla");
 
+// // leggere valori di prop
+// console.log(palla[nomePorp], palla.nomePorp);
 
-for (let i = 0; i < inputUser.length; i++) {
-    console.log(inputUser[i]);
+// sovra scrivere valori di prop
+palla.diametro = 5;
+
+console.log(palla);
+
+// aggiungere una prop
+
+palla.materiale = 'plastica';
+
+console.log(palla);
+
+// con un ciclo tiro fuori tutto ciò che mi serve
+for (let propPalla in palla) {
+    const messaggio = `il valore di  ${propPalla} è ${palla[propPalla]}`
+    console.log(messaggio);
 }
+
+
 
 
 
