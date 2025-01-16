83f9d3b98e90feddcff5943f90407d100167579c
Simone <info@simoneicardi.com>
2025-01-16 12:16:56 +0100

    es. utilizzo array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,33 +1,60 @@
-// creiamo il nostro oggetto
-const palla = {
-    'colore': ['red', 'blue'],
-    'tipo': 'pallina da golf',
-    'diametro': 3
-};
+// array di oggetti
+const classi = [
+    {
+        'nome': 'Classe#1',
+        'numeroStudenti': 10,
+    },
+    {
+        'nome': 'Classe#2',
+        'numeroStudenti': 12,
+    },
+    {
+        'nome': 'Classe#3',
+        'numeroStudenti': 15,
+    },
+    {
+        'nome': 'Classe#4',
+        'numeroStudenti': 9,
+    },
+];
 
-// const nomePorp = prompt("inserisci il nome di una prop della palla");
+// console.log(classi[1].nome, classi[1].numeroStudenti);
 
-// // leggere valori di prop
-// console.log(palla[nomePorp], palla.nomePorp);
+// console.table(classi);
 
-// sovra scrivere valori di prop
-palla.diametro = 5;
-
-console.log(palla);
-
-// aggiungere una prop
-
-palla.materiale = 'plastica';
-
-console.log(palla);
-
-// con un ciclo tiro fuori tutto ciò che mi serve
-for (let propPalla in palla) {
-    const messaggio = `il valore di  ${propPalla} è ${palla[propPalla]}`
-    console.log(messaggio);
+// creo un nuovo oggetto classe
+const nuovaClasse = {
+    'nome': 'Classe#5',
+    'numeroStudenti': 19,
 }
 
+// lo aggiungo alla lista delle classi
+classi.push(nuovaClasse);
+
+// console.table(classi);
 
 
 
+
+// tiriamo fuori tutto quello che c'è nella lista
+for (let i = 0; i < classi.length; i++) {
+
+    // elemento iesimo dell'array
+    let classeIesima = classi[i];
+
+    // console.log(classeIesima);
+
+    // estrapolo la prop nome dell'oggeto iesimo
+    // let nomeClassse = classeIesima.nome;
+
+    // console.log(nomeClassse);
+
+    // cilco all'interno dell'oggetto iesimo
+    for (let chiaveClasse in classeIesima) {
+        const mess = `Il valore di ${chiaveClasse} è ${classeIesima[chiaveClasse]}`
+        console.log(mess);
+
+    }
+
+}
 
