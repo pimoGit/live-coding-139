50d88ead5f98fdb718b33d59ee02d79bf45ef65a
Simone <info@simoneicardi.com>
2025-01-20 11:51:18 +0100

    gestione prev next carosello

--- js/script.js
+++ js/script.js
@@ -1,75 +1,115 @@
-// dati del team
-const teamMembers = [
+// # Fase di preparazione
+//  Preparo i dati su cui basare il carosello
+const pics = [
     {
-        name: 'Wayne Barnett',
-        role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg',
-    },
-    {
-        name: 'Angela Caroll',
-        role: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpg',
-    },
-    {
-        name: 'Walter Gordon',
-        role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg',
-    },
-    {
-        name: 'Angela Lopez',
-        role: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.jpg',
-    },
-    {
-        name: 'Scott Estrada',
-        role: 'Developer',
-        image: 'scott-estrada-developer.jpg',
-    },
-    {
-        name: 'Barbara Ramos',
-        role: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpg',
-    },
+        image: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Scandinavia\'s blend of nature and innovation.',
+    }, {
+        image: 'img/02.jpg',
+        title: 'Norvegia',
+        text: 'Fjords, mountains, and coastal charm in Nordic splendor.',
+    }, {
+        image: 'img/03.jpg',
+        title: 'Alaska',
+        text: 'Untamed wilderness and rugged beauty in the Last Frontier.',
+    }, {
+        image: 'img/04.jpg',
+        title: 'Gran Canyon',
+        text: 'Nature\'s masterpiece, a colorful tapestry of cliffs.',
+    }, {
+        image: 'img/05.jpg',
+        title: "Skyrim",
+        text: 'Epic Nordic fantasy with dragons and ancient magic.',
+    }
 ];
 
-// selezione elemento di output
-const listTeam = document.querySelector(".team-container");
-// console.log(listTeam);
+// selezione elemento gallery di output slides
+const containerGallery = document.querySelector('.gallery');
+console.log(containerGallery);
 
 
-let items = "";
-// output temporaneo con LI in UL
-for (let i = 0; i < teamMembers.length; i++) {
-    let menbroIesimo = teamMembers[i];
-    // console.log(menbroIesimo);
+// generiamo dinamicamente il blocco dei vari elementi di slide
 
-    // estrapoliamo i valori delle varie prop dell'oggetto
-    // let name = menbroIesimo.name;
-    // let role = menbroIesimo.role;
-    // let image = menbroIesimo.image;
+// variabile di accumulo dell'elento di output (stringa)
+let galleryElements = '';
 
-    const { name, role, image } = menbroIesimo;
+// ciclo su array e incremento di galleryElement con gli elementi di output
+for (let i = 0; i < pics.length; i++) {
+    let currentSlide = pics[i];
+    // console.log(currentSlide);
 
-    // console.log(name, role, image);
+    // destrutturazione oggetto slide
+    const { title, text, image } = currentSlide;
 
-    items += `    
-    <div class="team-card">
-        <div class="card-image">
-            <img src="img/${image}" alt="${name}" />
-        </div>
-        <div class="card-text">
-            <h3>${name}</h3>
-            <p>${role}</p>
-        </div>
-    </div>
-    `
-
-    console.log(items);
+    // aggiungo ad ogni iterazione una nuova slide
+    galleryElements += `
+    <figure>
+        <figcaption>
+            <h2>${title}</h2>
+            <h3>${text}</h3>
+        </figcaption>
+        <img alt="${title}" src="${image}">
+    </figure>
+    `;
+    // console.log(galleryElements);
 
 }
 
-// ouput
-listTeam.innerHTML = items;
+// inserisco le slide dentro gallery
+containerGallery.innerHTML = galleryElements;
+
+
+// gestione iniziale dell'indice della slide attiva (gestione active)
+// seleziono tutte le slide
+const slides = document.querySelectorAll('.gallery figure');
+console.log(slides);
+// variabile di gestione dell'indice per l'active
+let currentIndex = 0;
+// aggiungo la classe active al primo elemento
+slides[currentIndex].classList.add('active');
 
 
 
+// gestione navigazione da pulsanti
+
+// seleziono i bottoni
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+console.log(prevButton, nextButton);
+
+// gestione next
+nextButton.addEventListener('click', () => {
+    // rimuovo l'active dalla slide corrente
+    slides[currentIndex].classList.remove('active');
+
+    if (currentIndex < slides.length - 1) {
+        // aggiorno il valore dell'indice corrente
+        currentIndex = currentIndex + 1;
+    } else {
+        currentIndex = 0;
+    }
+
+    // aggiungo l'active alla slide nuova
+    slides[currentIndex].classList.add('active');
+
+}
+);
+
+// gestione prev
+prevButton.addEventListener('click', () => {
+    // rimuovo l'active dalla slide corrente
+    slides[currentIndex].classList.remove('active');
+
+    if (currentIndex > 0) {
+        // aggiorno il valore dell'indice corrente
+        currentIndex = currentIndex - 1;
+    } else {
+        currentIndex = slides.length - 1;
+    }
+
+    // aggiungo l'active alla slide nuova
+    slides[currentIndex].classList.add('active');
+
+}
+);
