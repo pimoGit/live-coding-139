987fd65ee0ea472d9a984c9aa02f6cbb292341f5
Simone <info@simoneicardi.com>
2025-01-21 12:13:16 +0100

    es. reale di tutilizzo metodi cicli su array di oggetti

--- js/script.js
+++ js/script.js
@@ -1,39 +1,65 @@
-const numeri = [1, 2, 3, 4, 5];
-
-// ricerca di un elemento pari
-const primoNumeroPari = numeri.find((numero) => {
-    return numero % 2 === 0;
-});
-
-
-
-
-// versione for tradizionale
-// let primoNumeroPari;
-
-// for (let i = 0; i < numeri.length && primoNumeroPari === undefined; i++) {
-//     let numero = numeri[i];
-//     if (numero % 2 === 0) {
-//         primoNumeroPari = numero;
-//     }
-// }
-
-
-console.log(primoNumeroPari); // 2
-
-
-//  es. di ricerca specifica su array di oggetti
-const people = [
-    { id: 4, name: 'Paolo', age: 32 },
-    { id: 3, name: 'Clelia', age: 34 },
+const students = [
+    {
+        id: 1,
+        name: 'Luca Bianchi',
+        age: 32,
+        class: '3C',
+        vote: 9
+    },
+    {
+        id: 2,
+        name: 'Andrea Rossi',
+        age: 18,
+        class: '4A',
+        vote: 7
+    },
+    {
+        id: 3,
+        name: 'Giovanni Verdi',
+        age: 33,
+        class: '5B',
+        vote: 7
+    },
+    {
+        id: 4,
+        name: 'Francesco Esposito',
+        age: 25,
+        class: '5B',
+        vote: 7
+    },
+    {
+        id: 5,
+        name: 'Matteo Ferri',
+        age: 57,
+        class: '5B',
+        vote: 10
+    },
 ];
 
-// trova la person con id 3
-const myPerson = people.find((person) => {
-    return person.id === 3;
+// Stampa in console i nomi di tutti gli studenti
+students.forEach((student) => {
+    console.log(student.name);
 });
 
-//RISULTATO: { id: 3, name: 'Clelia', age: 34 }
+// Crea un array con la lista  di tutti i nomi degli studenti
+const nomi = students.map((student) => {
+    return student.name;
+});
 
-console.log(myPerson, myPerson.name, myPerson.age);
+console.log(nomi);
 
+
+// Crea un array che contenga solo gli studenti dai 30 anni in più
+const over30 = students.filter((student) => {
+    return student.age >= 30;
+});
+
+console.log(over30);
+
+
+// Trova lo studente con id 4
+const studente4 = students.find((student) => {
+    return student.id === 4;
+});
+
+console.log(studente4);
