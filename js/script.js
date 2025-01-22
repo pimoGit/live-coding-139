332f1b885adc3999df05b8f033ef29eb9ede2a3e
Simone <info@simoneicardi.com>
2025-01-22 12:54:33 +0100

    es. da API array di numeri gioco dadi

--- js/script.js
+++ js/script.js
@@ -1,20 +1,43 @@
-// const numero = 7;
-// selezioniamo l'elemnto di output
-const outputEl = document.getElementById('outputNum');
-// console.log(outputEl);
+// selezioniamo gli elemnti di output
+const giocatore1 = document.getElementById('p1');
+const giocatore2 = document.getElementById('p2');
+const risultato = document.getElementById('result');
 
-const endpoint = 'https://flynn.boolean.careers/exercises/api/random/int';
+// console.log(giocatore1, giocatore2, risultato);
+
+// setto l'endpoint e i parametri relativi
+const numMin = 1;
+const numMax = 6;
+const numItems = 2;
+const endpoint = `https://flynn.boolean.careers/exercises/api/array/integers?min=${numMin}&max=${numMax}&items=${numItems}`;
 
 // faccio partire la richiesta Ajax verso l'API per ricevere il numero
 axios.get(endpoint)
     .then(responseObj => {
         //codice da eseguire in caso di successo
-        const result = responseObj.data;
-        let numero = result.response
-        console.log(numero);
+        const numbers = responseObj.data.response;
+        console.log(numbers);
+        // etraspolo i numeri dall'array
+        // const numGiocatore1 = numbers[0];
+        // const numGiocatore2 = numbers[1];
+        const [numGiocatore1, numGiocatore2] = numbers;
+        // const numGiocatore1 = 2;
+        // const numGiocatore2 = 2;
+        console.log(numGiocatore1, numGiocatore2);
 
         // output in pagina
-        outputEl.innerText = numero;
+        giocatore1.innerHTML = numGiocatore1;
+        giocatore2.innerHTML = numGiocatore2;
+
+        let messaggioGioco = "Pareggio!";
+
+        if (numGiocatore1 > numGiocatore2) {
+            messaggioGioco = "Ha vinto il primo giocatore";
+        } else if (numGiocatore2 > numGiocatore1) {
+            messaggioGioco = "Ha vinto il secondo giocatore";
+        }
+
+        risultato.innerHTML = messaggioGioco;
 
     })
     .catch(error => {
